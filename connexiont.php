83ca71d5c5b8de3374bc383f04e7de6c407fb209
JUDEGON <38042423+JUDEGON@users.noreply.github.com>
2019-03-21 15:28:21 +0100

    Add files via upload
    Projet presque finit

---
+++ connexiont.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "SUTZUUUU";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// username and password sent from form
+$myusername = $_POST['email'];
+$mypassword = $_POST['password'];
+$sql = "SELECT IDU, nom, prenom FROM Utilisateur WHERE mail = '$myusername' AND mdp = '$mypassword'";
+echo $sql;
+$result = $conn->query($sql);
+$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+$ID = $row["IDU"];
+$nom = $row["nom"];
+$prenom = $row["prenom"];
+$count = mysqli_num_rows($result);
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count == 1) {
+    $_SESSION['login_user'] = $myusername;
+    $_SESSION['id'] = $ID;
+    $_SESSION['nom'] = $nom;
+    $_SESSION['prenom'] = $prenom;
+    echo "success";
+        header('Location: feed.php');
+}
+else
+{
+    echo "Your Login Name or Password is invalid";
+    header('Location: index.php');
+    $_SESSION['message'] = "tia fait de la moula le S";
+
+}
+?>
---
+++ feed.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Fil d'actualité</title>
+
+    <link rel="stylesheet" href="stylefeed.css">
+  </head>
+  <body>
+
+  	<div class="nav d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+	  <img src="sutsu.png" class="my-0 mr-md-auto font-weight-normal">
+	  <nav class="my-2 my-md-0 mr-md-3 ">
+		    <a class="p-2 text-dark" href="feed.php">Fil d'actualité</a>
+		    <a class="p-2 text-dark" href="mesquestions.php">Mes questions</a>
+	  </nav>
+	</div>
+
+	<div class="jumbotron">
+	    <div class="container">
+
+	      <h1 class="display-4">Contexte</h1>
+	      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+
+	      <h1 class="display-4">Question</h1>
+	      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+
+
+	      <div class="row">
+		    <div class="col">
+		      <a class="btn btn-outline-primary btn-block" href="#" role="button">Oui</a>
+		    </div>
+		    <div class="col">
+		      <a class="btn btn-outline-success btn-block" href="#" role="button">Non</a>
+		    </div>
+		  </div>
+
+	    </div>
+    </div>
+
+
+
+	<footer class="footer text-muted bd-footer">
+	  <div class="container">
+	    <p class="float-right">
+	      <a href="#">Déconnexion</a>
+	    </p>
+	    <p>Sutsu : réalisé par l'équipe PPE n°1810. Besoin de plus d'informations ? Contacter par mail : <a href="mailto:hugo.fridlanski@edu.ece.fr">hugo.fridlanski@edu.ece.fr</a></p>
+	  </div>
+	</footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
